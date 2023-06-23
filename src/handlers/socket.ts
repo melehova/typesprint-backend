04b0fe3a0e3a68aa31b35c6d44328fcd35ced6af
vmelekhova <veronika0melehova@gmail.com>
2023-06-23 16:51:49 +0300

    update room leaving handling

--- src/handlers/socket.ts
+++ src/handlers/socket.ts
@@ -1,11 +1,12 @@
 import { Server, Socket } from 'socket.io';
-import { addMember, getRoomInfo, removeMember, validateRoom } from '../helpers/room';
+import { addMember, getRoomInfo, removeMember, validateRoom, isUserHost, shiftHost } from '../helpers/room';
 import cookie from 'cookie';
 
 export const roomHandler = (io: Server, socket: Socket): void => {
+    const { 'user-id': userId, 'access-token': accessToken } = cookie.parse(socket.handshake.headers.cookie!);
+
 
     socket.on('joinRoom', async (roomId: string) => {
-        const { 'user-id': userId, 'access-token': accessToken } = cookie.parse(socket.handshake.headers.cookie!);
 
         if (!(await validateRoom(roomId))) {
             socket.emit('invalidRoom');
@@ -27,16 +28,24 @@ export const roomHandler = (io: Server, socket: Socket): void => {
         })
     })
 
+    socket.on('gameStart', async (roomId: string) => {
+        // TODO
+        // update status
+        // get words
+        // send words and updated room info
+
+    })
+
 
 };
 
 const leaveRoomEventHandler = async (io: Server, socket: Socket, roomId: string, userId: string) => {
     socket.leave(roomId);
     await removeMember(roomId, userId);
-    // TODO
-    // check if room is empty and delete if true
 
-    // check if user host and shift host if true
+    if (await isUserHost(roomId, userId)) {
+        await shiftHost(roomId);
+    }
 
     const roomInfo = await getRoomInfo(roomId);
     io.in(roomId).emit('roomMembersUpdate', roomInfo);
--- src/helpers/room.ts
+++ src/helpers/room.ts
@@ -6,7 +6,6 @@ export const createRoom = async (roomId: string, userId: string) => {
     try {
         await cacheManager.hset(`room:${roomId}`, 'host', userId);
         await cacheManager.hset(`room:${roomId}`, 'state', GAME_STATE.LOBBY);
-        // await addMember(roomId, userId, accessToken!);
     } catch (error) {
         throw new Error('Error creating room');
     }
@@ -19,13 +18,10 @@ export const validateRoom = async (roomId: string): Promise<number> => {
 export const addMember = async (roomId: string, userId: string, accessToken: string): Promise<number> => {
     try {
         const { name, photo } = await getProfileData(accessToken);
-        // return await cacheManager.sadd(`roomPlayers:${roomId}`, { userId, name, photo });
         return await cacheManager.hset(`roomPlayers:${roomId}`, userId, { userId, name, photo });
     } catch (error: any) {
-        // console.error(error?.data?.error || error?.message || error)
         console.error(error?.response?.data?.error)
         throw new Error('Error joining room');
-
     }
 }
 
@@ -43,11 +39,11 @@ export const accessToRoom = async (roomId: string, userId: string) => {
     return (host === userId);
 }
 
-export const shiftHost = async (roomId: string) => { // TODO remake for new structure
-    // Check if room is not empty
+export const shiftHost = async (roomId: string) => {
     const key = `roomPlayers:${roomId}`;
-    if (await cacheManager.redis.sCard(key) - 1) {
-        const newHostId = await cacheManager.redis.sRandMember(key);
+
+    if (await cacheManager.redis.hLen(key)) {
+        const newHostId = await cacheManager.redis.hRandField(key);
         await cacheManager.hset(`room:${roomId}`, 'host', newHostId);
         return newHostId;
     }
@@ -62,4 +58,8 @@ export const getRoomInfo = async (roomId: string) => {
     const members = await cacheManager.hgetall(`roomPlayers:${roomId}`);
     const room = await cacheManager.hgetall(`room:${roomId}`);
     return { members, ...room, id: roomId }
+}
+
+export const isUserHost = async (roomId: string, userId: string) => {
+    return await cacheManager.hget(`room:${roomId}`, 'host') === userId
 }
