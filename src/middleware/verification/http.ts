d7aab16e8e29d5c3618f51e1123e8873a8bdf3dd
vmelekhova <veronika0melehova@gmail.com>
2023-06-23 19:20:35 +0300

    clear console logs

--- src/middleware/verification/http.ts
+++ src/middleware/verification/http.ts
@@ -5,16 +5,13 @@ export default async (request: FastifyRequest, reply: FastifyReply, done: HookHa
     try {
         const { 'user-id': userId = '', 'access-token': accessToken = '' } = request.cookies;
 
-        // Verify and validate the token
         const isValidToken = await validateToken(userId, accessToken);
 
         if (!isValidToken && request.url !== '/api/logout') {
-            // Handle invalid token case
             reply.code(401).send({ error: 'Invalid token' });
             return;
         }
 
-        // Set the updated access token in the response cookies
         reply.setCookie('access-token', accessToken, {
             path: '/',
             httpOnly: true,
